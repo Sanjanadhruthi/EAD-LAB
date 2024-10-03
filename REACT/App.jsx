d636f913f0d32754a110140aab14c0843bf8ac05
Sanjanadhruthi <sanjanadhruthi@gmail.com>
2024-10-03 15:14:08 +0530

    Add files via upload

---
+++ REACT/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import HelloWorld from './components/HelloWorld'
+import Greeting from './components/Greeting'
+import Counter from './components/Counter'
+import PasswordStrengthChecker from './components/PasswordStrengthChecker'
+import Timer from './components/Timer'
+import hooks from './components/hooks'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <hooks />
+    </>
+  )
+}
+
+export default App
