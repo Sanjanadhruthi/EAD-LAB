fb9ec09e3c0adc7424dd9f67e9b6cb111fbee4f0
Sanjanadhruthi <sanjanadhruthi@gmail.com>
2024-10-18 16:33:25 +0530

    Add files via upload

--- postman/server1.js
+++ postman/server1.js
@@ -1,1 +1,34 @@
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
+app.use(express.json())
+
+const posts = [
+  { name: "CBIT", title: "Welcome to CBIT" },
+  { name: "MGIT", title: "Welcome to MGIT" },
+  { name: "VBIT", title: "Welcome to VBIT" }
+]
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1]
+  if (!token) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+app.post('/login', (req, res) => {
+  const accessToken = jwt.sign({ name: req.body.username }, process.env.ACCESS_TOKEN)
+  res.json({ accessToken })
+})
+
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.name === req.user.name))
+})
+
+app.listen(3000)
